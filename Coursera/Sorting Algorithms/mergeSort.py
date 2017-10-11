89fe5cee5dd97b2264bb00b281c6c8d14bfe7153
sobriquette <11lin.m7@gmail.com>
2017-10-11 14:58:47 -0400

    add implementation for iterative mergesort

--- Coursera/Sorting Algorithms/mergeSort.py
+++ Coursera/Sorting Algorithms/mergeSort.py
@@ -37,9 +37,9 @@ def merge(arr, low, mid, high):
 		j += 1
 		k += 1
 
-def merge_sort(arr, low, high):
+def merge_sort_recursive(arr, low, high):
 	"""
-		Implementation of mergesort
+		Recursive implementation of mergesort
 		type: list
 		rtype: list
 
@@ -51,3 +51,26 @@ def merge_sort(arr, low, high):
 		merge_sort(arr, low, mid)
 		merge_sort(arr, mid + 1, high)
 		merge(arr, low, mid, high)
+
+def merge_sort_iterative(arr, n):
+	"""
+		Iterative implementation of mergesort
+		type: list
+		rtype: list
+
+		runtime: O(nlogn)
+		space: O(n) for temp arrays in merge()
+	"""
+
+	# Merges subarrays in bottom up manner
+	# First merge subarrays of size 1 to create sorted subarrays of size 2,
+	# then merge subarrays of size 2 to subarrays of size 4...etc.
+	for size in range(1, n-1, size * 2):
+		# Pick starting point of different subarrays of current size
+		for left in range(0, n-1, left + (size * 2)):
+			# Find ending point of left subarray
+			# mid + 1 is starting point of right
+			mid = left + size - 1
+			right = min((left + 2*size - 1), n-1)
+			# merge subarrays
+			merge(arr, left, mid, right)
