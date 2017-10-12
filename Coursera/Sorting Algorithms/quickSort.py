3480c880d9c702fdf6cc7a341463d285e83020dd
Melody Lin <11lin.m7@gmail.com>
2017-10-11 23:17:25 -0400

    add gitignore and updates to quicksort

--- Coursera/Sorting Algorithms/quickSort.py
+++ Coursera/Sorting Algorithms/quickSort.py
@@ -1,39 +1,73 @@
-def quicksort(arr, low, high):
+def partition(arr, low, high):
+	"""
+		Partitioning using last element
+	"""
+	pivot = arr[high]
+	# index of smaller element
+	i = low - 1
+
+	for j in range(low, high):
+		if arr[j] <= pivot:
+			i += 1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[i + 1], arr[high] = pivot, arr[i + 1] 
+
+	return (i + 1)
+
+def quicksort_recursive(arr, low, high):
 	"""
 		Recursive implementation of quicksort
 		type: list
 		rtype: list
 
 		runtime: O(n^2) worst case b/c always picking last element
-		space: O(1)
+		space: O(n)
 	"""
 
-	def partition():
-		"""
-			Partitioning using last element
-		"""
-		pivot = arr[high]
-		# index of smaller element
-		i = low - 1
-
-		for j in range(low, high):
-			if arr[j] <= pivot:
-				i += 1
-				arr[i], arr[j] = arr[j], arr[i]
-
-		arr[i + 1], arr[high] = pivot, arr[i + 1] 
-
-		return (i + 1)
-
 	while low < high:
-		pi = partition()
+		pi = partition(arr, low, high)
 
 		if (pi - low) < (high - pi):
-			quicksort(arr, low, (pi - 1))
+			quicksort_recursive(arr, low, (pi - 1))
 			low = pi + 1
 		else:
-			quicksort(arr, (pi + 1), high)
+			quicksort_recursive(arr, (pi + 1), high)
 			high = pi - 1
+
+	return arr
+
+def quicksort_iterative(arr, low, high):
+	"""
+		Iterative implementation of quicksort
+		type: list
+		rtype: list
+
+		runtime: O(n^2)
+		space: O(n)
+	"""
+
+	stack = [0] * (high - low + 1)
+	stack.append(low)
+	stack.append(high)
+
+	while len(stack) >= 2:
+		high = stack.pop()
+		low = stack.pop()
+
+		pi = partition(arr, low, high)
+
+		# push left side onto stack if there are elements
+		# on left side of pivot
+		if pi - 1 > low:
+			stack.append(low)
+			stack.append(pi - 1)
+
+		# push right side onto stack if there are elements
+		# on right side of pivot
+		if pi + 1 < high:
+			stack.append(pi + 1)
+			stack.append(high)
 
 	return arr
 
@@ -45,4 +79,6 @@ if __name__=="__main__":
 			break
 
 		arr = list(map(int, response.split()))
-		print(quicksort(arr, 0, len(arr) - 1))
+		print(quicksort_recursive(arr, 0, len(arr) - 1) == \
+			  quicksort_iterative(arr, 0, len(arr) - 1))
+
