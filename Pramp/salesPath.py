90dcaed150576ab536b6d8188ae8edeedead806c
Melody Lin <11lin.m7@gmail.com>
2018-01-23 15:52:54 -0500

    add iterative solution for sales path problem

--- Pramp/salesPath.py
+++ Pramp/salesPath.py
@@ -4,7 +4,7 @@ class Node:
 		self.children = []
 		self.parent = None
 
-def get_cheapest_path(root):
+def get_cheapest_path_rec(root):
 	if not root.children:
 		return root.cost
 
@@ -14,3 +14,18 @@ def get_cheapest_path(root):
 		min_cost = min(min_cost, curr_cost)
 
 	return min_cost + root.cost
+
+def get_cheapest_cost_it(root):
+	stack = [(root, root.data)]
+	min_path = float('inf')
+
+	while stack:
+		node, curr_cost = stack.pop()
+		
+		if not node.children:
+			min_path = min(min_path, curr_cost)
+		else:
+			for child in node.children:
+				stack.append((child, child.data + curr_cost))
+
+	return min_path
